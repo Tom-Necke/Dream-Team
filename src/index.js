cbb0be85c52a8fccf11b08048eecbaf70b0f6ea4
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-05-24 19:22:04 +0200

    Designissues suggestions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import chalk from "chalk";
 import axios from "axios";
 import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
 
 let [userUrl] = process.argv.slice(2);
 const encodedParams = new URLSearchParams();
@@ -18,32 +19,48 @@ const options = {
   data: encodedParams
 };
 
+let lines = "-------------------------------------------------------------------------------------------------------------------";
 let duck = gradient('orange', 'yellow').multiline([
   "  __",
   "<(o )___",
   " ( ._> /",
   "  `---'",
 ].join('\n'));
+  //"    __  ",
+  //"___( o)>",
+  //"\ <_. )",
+  //" '---´",
 
 
-let logo = gradient('blue', 'yellow').multiline([
-  "██████╗░██████╗░███████╗░█████╗░███╗░░░███╗  ████████╗███████╗░█████╗░███╗░░░███╗",
-  "██╔══██╗██╔══██╗██╔════╝██╔══██╗████╗░████║  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║",
-  "██║░░██║██████╔╝█████╗░░███████║██╔████╔██║  ░░░██║░░░█████╗░░███████║██╔████╔██║",
-  "██║░░██║██╔══██╗██╔══╝░░██╔══██║██║╚██╔╝██║  ░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║",
-  "██████╔╝██║░░██║███████╗██║░░██║██║░╚═╝░██║  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║",
-  "╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝",
+let logo = gradient('blue', 'red').multiline([
+"            ██████╗░██████╗░███████╗░█████╗░███╗░░░███╗  ████████╗███████╗░█████╗░███╗░░░███╗",
+"            ██╔══██╗██╔══██╗██╔════╝██╔══██╗████╗░████║  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║",
+"            ██║░░██║██████╔╝█████╗░░███████║██╔████╔██║  ░░░██║░░░█████╗░░███████║██╔████╔██║",
+"            ██║░░██║██╔══██╗██╔══╝░░██╔══██║██║╚██╔╝██║  ░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║",
+"            ██████╔╝██║░░██║███████╗██║░░██║██║░╚═╝░██║  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║",
+"            ╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝",
 ].join('\n'));
 
 
 axios.request(options).then(function (response) {
   console.log();
-  console.log(`Hello Nerd! Thank you for your input: ${chalk.blue.bgRed(userUrl)}! Interesting! Our suggestion is following! `)
-  console.log(`${duck} ${gradient("lightBlue", "yellow", "red")("The return of the Shorty:")} ${chalk.bgBlueBright.red(response.data.result_url)}!`);
+  console.log(lines)
+  console.log(`
+  
+            Hello Nerd!
+
+            Thank you for your input: ${chalk.bgRed.blue(userUrl)}! Interesting! Our suggestion is following! `)
+  console.log(`${duck} ${gradient("lightBlue", "yellow", "red")("    The return of the shorty:")} ${chalk.bgBlueBright.red(response.data.result_url)}!`);
   console.log();
-  console.log("Shorty-App is a solution by:");
+  console.log(`
+
+            Shorty-App is a solution by:`);
   console.log();
   console.log(logo);
+  console.log(`             © Christoph, Joachim, Tom
+  
+  `);
+  console.log(lines);
 }).catch(function (error) {
 	console.error(error);
 });
